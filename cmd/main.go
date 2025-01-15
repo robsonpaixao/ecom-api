5a46ed4e5732fdbeee754bff70f2dd330397bc73
Robson Paixão <robson.emanuelpx@gmail.com>
2025-01-15 00:05:30 -0300

    initial db configs

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/robsonpaixao/ecom-api/cmd/api"
+	"github.com/robsonpaixao/ecom-api/configs"
+	"github.com/robsonpaixao/ecom-api/db"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	cfg := mysql.Config{
+		User:                 configs.Envs.DBUser,
+		Passwd:               configs.Envs.DBPassword,
+		Addr:                 configs.Envs.DBAddress,
+		DBName:               configs.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	}
+
+	db, err := db.NewMysqlStorage(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DB: Successfully connected!")
 }
